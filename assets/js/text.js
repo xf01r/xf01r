e08db3e651e1635ba48db085119c559aadfbe686
xf01r <84892930+xf01r@users.noreply.github.com>
2021-05-27 08:05:55 +0300

    Add files via upload

---
+++ assets/js/text.js
@@ -0,0 +1,28 @@
+$(function () {
+    var title = document.title,
+        animSeq = ["|"],
+        animIndex = 0,
+        titleIndex = 0;
+    function doInverseSpinZeroPitch() {
+        var loadTitle = title.substring(0, titleIndex);
+        if (titleIndex > title.length) {
+            animIndex = 0;
+            titleIndex = 0
+        }
+        if (animIndex > 1) {
+            titleIndex++;
+            animIndex = 0
+        }
+        document.title = loadTitle + animSeq[0];
+        animIndex++
+    }
+    window.setInterval(doInverseSpinZeroPitch, 150);
+});
+$(function () {
+    $(".typed").typed({
+        strings: ['Unik', "Кто это?", "Python программист", "Junior", "Недохакер"],
+        typeSpeed: 120,
+        loop: true,
+        cursorChar: "|",
+    });
+});
